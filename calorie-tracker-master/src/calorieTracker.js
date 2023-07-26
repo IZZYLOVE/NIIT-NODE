118b5e03ce0e43aeecbebb5e79964967bf1b45b2
IZZYLOVE <izzylovu@gmail.com>
2023-07-26 11:26:34 +0100

    test passed locally

--- calorie-tracker-master/src/calorieTracker.js
+++ calorie-tracker-master/src/calorieTracker.js
@@ -2,7 +2,14 @@ const calculateWeightLostInAMonth = (cycling,running,swimming,extraCalorieInTake
    let weightLostInAMonth = 0;
 
    // write logic here 
+   let totalcal = cycling + running + swimming - extraCalorieInTake
+   weightLostIn = (totalcal / 1000 * 6).toFixed(1)
 
+   if (weightLostIn < 0 || cycling <= 0 || running <= 0 || swimming <= 0) {
+      weightLostInAMonth = -1
+   } else {
+      weightLostInAMonth = Number(weightLostIn)
+   }
    return weightLostInAMonth;
    
 }
